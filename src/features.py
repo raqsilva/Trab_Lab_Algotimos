ecf2e6a1242e5cccdc2f224fd7959f6fb58f0c24
raqsilva <raqsilva8@hotmail.com>
2014-12-27 22:18:32 +0000

    creating functions

--- src/features.py
+++ src/features.py
@@ -1,27 +1,32 @@
 from Bio import SeqIO 
 from Bio import Entrez
 import shutil
+import os.path
 
 #[0:246000]
 
 #fetching [0:246000] genome from Neisseria gonorrhoeae FA 1090 chromosome ncbi
 #VERSION: NC_002946.2   GI:59800473
 
-Entrez.email = "pg27668@alunos.uminho.pt"
-handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", seq_start="1", seq_stop="246000", id="59800473")
-file=open("sequencia.gb","w")#creating a GenBank file
-file.write(handle.read())
-file.close()
-handle.close()
+def get_genome_zone(start,stop,filename):
+    #start=1
+    #stop=246000
+    #filename=filename +".gb"
+    Entrez.email = "pg27668@alunos.uminho.pt"
+    handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", seq_start="1", seq_stop="246000", id="59800473")
+    file=open("sequencia.gb","w")#creating a GenBank file
+    file.write(handle.read())
+    file.close()
+    handle.close()
+    #moving the file to another directory
+    src = "D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\src\\sequencia.gb"
+    dst = "D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\res"
+    shutil.move(src, dst)
+    record = SeqIO.read("../res/sequencia.gb", "genbank") 
+    return record
 
-#moving the file to another directory
-src = "D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\src\\sequencia.gb"
-dst = "D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\res"
-shutil.move(src, dst)
 
-#getting my file 
-record = SeqIO.read("../res/sequencia.gb", "genbank") 
-
+def features
 #analysing the features
 featcds = [ ] 
 for i in range(len(record.features)):
@@ -48,6 +53,17 @@ for k in featcds:
 
 x=record.features[1]
 print(x.qualifiers)
+
+
+def teste():
+    start=input(print("Insira o inicio da sua zona do genoma:"))
+    stop=input(print("Insira o fim da sua zona do genoma:"))
+    filename=input(print("Insira nome do ficheiro: "))
+    while os.path.isfile("D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\src\\"+filename):
+        filename=input(print("Insira outro nome: "))
+        False
+    record=get_genome_zone(start,stop,filename)
+    
 
 
  
