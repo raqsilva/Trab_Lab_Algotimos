8cadc33017763598748bd9f5a151b35b7d19f34d
raqsilva <raqsilva8@hotmail.com>
2014-12-27 22:28:53 +0000

    new function

--- src/features.py
+++ src/features.py
@@ -3,14 +3,12 @@ from Bio import Entrez
 import shutil
 import os.path
 
-#[0:246000]
+#[0:246000] my zone
 
-#fetching [0:246000] genome from Neisseria gonorrhoeae FA 1090 chromosome ncbi
+#fetching genome zone from Neisseria gonorrhoeae FA 1090 chromosome ncbi
 #VERSION: NC_002946.2   GI:59800473
 
 def get_genome_zone(start,stop,filename):
-    #start=1
-    #stop=246000
     #filename=filename +".gb"
     Entrez.email = "pg27668@alunos.uminho.pt"
     handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", seq_start="1", seq_stop="246000", id="59800473")
@@ -26,49 +24,50 @@ def get_genome_zone(start,stop,filename):
     return record
 
 
-def features
-#analysing the features
-featcds = [ ] 
-for i in range(len(record.features)):
-    my_cds = record.features[i]
-    if record.features[i].type == "CDS": 
-        featcds.append(i)
-        if "locus_tag" in my_cds.qualifiers:
-            print(my_cds.qualifiers["locus_tag"])
-        else:
-            print("Nao contem locus_tag!")
-            
-        if "product" in my_cds.qualifiers:
-            print(my_cds.qualifiers["product"])
-        else:
-            print("Nao contem produtos!")
-       
-        if "note" in my_cds.qualifiers:
-            print(my_cds.qualifiers["note"])
-        else:
-            print("Nao contem nota!")
-for k in featcds: 
-    print (record.features[k].location)
+def features(record):
+    #analysing the features
+    featcds = [ ] 
+    for i in range(len(record.features)):
+        my_cds = record.features[i]
+        if record.features[i].type == "CDS": 
+            featcds.append(i)
+            if "locus_tag" in my_cds.qualifiers:
+                print(my_cds.qualifiers["locus_tag"])
+            else:
+                print("Nao contem locus_tag!")
+                
+            if "product" in my_cds.qualifiers:
+                print(my_cds.qualifiers["product"])
+            else:
+                print("Nao contem produtos!")
+                
+            if "note" in my_cds.qualifiers:
+                print(my_cds.qualifiers["note"])
+            else:
+                print("Nao contem nota!")
+    for k in featcds: 
+        print (record.features[k].location)
 
 
-x=record.features[1]
-print(x.qualifiers)
+#x=record.features[1]
+#print(x.qualifiers)
 
 
 def teste():
-    start=input(print("Insira o inicio da sua zona do genoma:"))
-    stop=input(print("Insira o fim da sua zona do genoma:"))
-    filename=input(print("Insira nome do ficheiro: "))
+    start=str(input(print("Insira o inicio da sua zona do genoma:")))
+    stop=str(input(print("Insira o fim da sua zona do genoma:")))
+    filename=str(input(print("Insira nome do ficheiro: "))+".gb")
     while os.path.isfile("D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\src\\"+filename):
         filename=input(print("Insira outro nome: "))
         False
     record=get_genome_zone(start,stop,filename)
+    features(record)
     
 
 
  
-#if __name__ == "__main__":
-#    teste()
+if __name__ == "__main__":
+    teste()
     
 
 #for feat in record.features: 
