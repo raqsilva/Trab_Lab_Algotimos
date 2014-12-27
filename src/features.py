ac5f851626dd193a88163718d30393e48c4825a0
raqsilva <raqsilva8@hotmail.com>
2014-12-27 21:46:42 +0000

    fetching ncbi info anda saving to gb file

--- src/features.py
+++ src/features.py
@@ -1,8 +1,28 @@
 from Bio import SeqIO 
+from Bio import Entrez
+import shutil
+
 #[0:246000]
 
-record = SeqIO.read("../res/sequence.gb", "genbank") 
+#fetching [0:246000] genome from Neisseria gonorrhoeae FA 1090 chromosome ncbi
+#VERSION: NC_002946.2   GI:59800473
 
+Entrez.email = "pg27668@alunos.uminho.pt"
+handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", seq_start="1", seq_stop="246000", id="59800473")
+file=open("sequencia.gb","w")#creating a GenBank file
+file.write(handle.read())
+file.close()
+handle.close()
+
+#moving the file to another directory
+src = "D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\src\\sequencia.gb"
+dst = "D:\\Documentos\\GitHub\\Trab_Lab_Algotimos\\res"
+shutil.move(src, dst)
+
+#getting my file 
+record = SeqIO.read("../res/sequencia.gb", "genbank") 
+
+#analysing the features
 featcds = [ ] 
 for i in range(len(record.features)):
     my_cds = record.features[i]
@@ -38,20 +58,4 @@ print(x.qualifiers)
 #for feat in record.features: 
  #   print (str(feat))
 
-"""
-def index_genbank_features(gb_record, feature_type, qualifier) :
-    answer = dict()
-    for (index, feature) in enumerate(gb_record.features) :
-        if feature.type==feature_type :
-            if qualifier in feature.qualifiers :
-                #There should only be one locus_tag per feature, but there
-                #are usually several db_xref entries
-                for value in feature.qualifiers[qualifier] :
-                    if value in answer :
-                        print "WARNING - Duplicate key %s for %s features %i and %i" \
-                           % (value, feature_type, answer[value], index)
-                    else :
-                        answer[value] = index
-    return answer
-"""
 
