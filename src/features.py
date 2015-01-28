bd48f76ce7dad96b34ef1b91327f12848f3b94d1
raqsilva <raqsilva8@hotmail.com>
2015-01-28 13:10:37 +0000

    getting all the info from pseudogenes qualifiers

--- src/features.py
+++ src/features.py
@@ -125,15 +125,45 @@ def gene_ID_GI(record,locus_tag):
                     return x[0],x[1]
         
 
-#Getting pseudogenes
+#Getting pseudogenes locus_tag
 def pseudogenes(record):
-    genes=[]
+    locus=[]
     for i in range(len(record.features)):
         my_gene = record.features[i]
-        if my_gene.type == "gene": 
-            if "pseudogene" in my_gene.qualifiers:
-                genes.append(my_gene.qualifiers["db_xref"][0])
-    return genes
+        if my_gene.type == "gene":
+             if "pseudogene" in my_gene.qualifiers:
+                locus.append(my_gene.qualifiers["locus_tag"][0])
+    return locus
+
+
+def location_pseudo(record,locus_tag):
+    featcds = [ ]
+    for i in range(len(record.features)):
+        if record.features[i].type == "gene":
+            if record.features[i].qualifiers["locus_tag"][0]==str(locus_tag): 
+                featcds.append(i)
+    for k in featcds: 
+        return record.features[k].location
+
+
+def pseudogeneID(record,locus_tag):
+    for i in range(len(record.features)):
+        my_gene = record.features[i]
+        if my_gene.type == "gene":
+            if my_gene.qualifiers["locus_tag"][0]==str(locus_tag): 
+                if "pseudogene" in my_gene.qualifiers:
+                    return my_gene.qualifiers["db_xref"][0]
+
+
+def info_pseudogenes(record,locus):
+    lista=[]
+    for i in range(len(locus)):
+        lista.append([])
+        lista[i].append(locus[i])
+        lista[i].append(location_pseudo(record,locus[i]))
+        lista[i].append(pseudogeneID(record,locus[i]))
+    return lista   
+    
 
 
 #Getting genes names
@@ -306,7 +336,8 @@ def menu(record):
             loc=tRNA(record)
             print(info_tRNA(record,loc))
         elif ans=="5":
-            print(pseudogenes(record))
+            locus=pseudogenes(record)
+            print(info_pseudogenes(record,locus))
         elif ans=="6":
             print(hypoth_proteins(record))
         elif ans=="7":
