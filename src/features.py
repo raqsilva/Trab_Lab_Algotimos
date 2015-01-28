772e37a7cb9f9f1989ea9d6bd95b4b9965c6eb86
raqsilva <raqsilva8@hotmail.com>
2015-01-28 12:38:38 +0000

    getting all the info from tRNA qualifier

--- src/features.py
+++ src/features.py
@@ -172,17 +172,59 @@ def translation(record,protein_ID):
                         print( "Nao ha traducao")
                     
 
-#information from tRNA             
+#locus from tRNA             
 def tRNA(record):
-    trna=[]
+    #trna=[]
     locus=[]
     for i in range(len(record.features)):
         my_trna = record.features[i]
         if my_trna.type == "tRNA":
-            trna.append(my_trna.qualifiers["product"][0])
+            #trna.append(my_trna.qualifiers["product"][0])
             locus.append(my_trna.qualifiers["locus_tag"][0])
-    return trna,locus
-  
+    return locus
+
+
+#gets geneID form tRNA
+def gene_ID_tRNA(record,locus_tag):
+    for i in range(len(record.features)):
+        my_gene = record.features[i]
+        if my_gene.type == "tRNA":             
+            if my_gene.qualifiers["locus_tag"][0]==str(locus_tag):    
+                x=my_gene.qualifiers["db_xref"]
+                return x[0]
+                    
+
+#gets product from tRNA
+def product_tRNA(record,locus_tag):
+    for i in range(len(record.features)):
+        my_cds = record.features[i]
+        if my_cds.type == "tRNA":
+            if my_cds.qualifiers["locus_tag"][0]==str(locus_tag):  
+                return my_cds.qualifiers["product"][0]
+                    
+
+#Gets location strand from tRNA
+def location_tRNA(record,locus_tag):
+    featcds = [ ]
+    for i in range(len(record.features)):
+        if record.features[i].type == "tRNA":
+            if record.features[i].qualifiers["locus_tag"][0]==str(locus_tag): 
+                featcds.append(i)
+    for k in featcds: 
+        return record.features[k].location
+
+
+#Gets all the info from tRNA                
+def info_tRNA(record,locus):
+    lista=[]
+    for i in range(len(locus)):
+        lista.append([])
+        lista[i].append(locus[i])
+        lista[i].append(gene_ID_tRNA(record,locus[i]))
+        lista[i].append(product_tRNA(record,locus[i]))
+        lista[i].append(location_tRNA(record,locus[i]))
+    return lista   
+    
 
 #Searching articles from PubMed DB referring to my organism and a gene
 def DB_pubmed(gene):
@@ -261,7 +303,8 @@ def menu(record):
             prot_ID=str(input("Protein ID: "))
             print(translation(record,prot_ID))
         elif ans=="4":
-            print(tRNA(record))
+            loc=tRNA(record)
+            print(info_tRNA(record,loc))
         elif ans=="5":
             print(pseudogenes(record))
         elif ans=="6":
