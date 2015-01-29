b0d66d346e8073c2d0686094bbf57efe05cc4239
Danielbrag <danielbraga@live.com.pt>
2015-01-29 23:39:10 +0000

    localização testes

--- testes/untitled0.py
+++ testes/untitled0.py
@@ -1,17 +1,10 @@
-
- 
-#from Bio.SwissProt import KeyWList
-from Bio.ExPASy import Enzyme
-
-
-handle = open("uniprot.txt")
-records = Enzyme.parse(handle)
-x=[]
-for record in records:
-    x.append(record['CC'])
-handle.close()
-print(x)
-#y=str(x[0])
-##m=y.split("         ")
-##print("nome",m[0],"revisao",m[1].replace)
-#print (y)
+handle = open("teste.txt").readlines()
+text=str(handle)
+string='SUBCELLULAR LOCATION:'
+lista=[]
+#for i in range (len(text)):
+y=text.split(string)
+m=y[1]
+lista.append(m[0:10])
+    
+print(lista)
