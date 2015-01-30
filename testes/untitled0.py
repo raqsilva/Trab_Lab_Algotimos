dc2f33700f05c7659f299df7886b833e8753f58b
Danielbrag <danielbraga@live.com.pt>
2015-01-30 17:30:24 +0000

    #name , status of review and size

--- testes/untitled0.py
+++ testes/untitled0.py
@@ -4,28 +4,17 @@ Created on Thu Jan 29 23:45:12 2015
 
 @author: Danielbraga
 """
-
-def proteinlocation():
+#name , status of review and size
+def review():
     handle = open("uniprot.txt").readlines()
-    uniprotid='AC   '
+    status='ID   '
+    aa='AA.'
+    res=[]
     
-    mystr = 'SUBCELLULAR LOCATION:'
-    location=[]
-    uniprot=[]
-    lista=[]
     
     for i in handle:
-        if mystr in i:
-            y=i.split(" ")            
-            location.append(y[6] )
+        if status and aa in i:  
+            res.append(i) 
+    return res
     
-    for j in handle:
-        if uniprotid in j:
-            uniprot.append(j)
-    x=len(location)        
-    for k in range(x):
-        lista.append(((uniprot[k]+" "+location[k])))
-        
-    return lista
-    
-x=(proteinlocation())
+print(review())
