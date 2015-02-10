0e3aae1964400936aefa59ddad4542aeb11afcf8
Danielbrag <danielbraga@live.com.pt>
2015-02-10 14:16:52 +0000

    excel code final

--- src/get_protein_function.py
+++ src/get_protein_function.py
@@ -1,39 +1,17 @@
 # -*- coding: utf-8 -*-
-
-#import os.path
-#from openpyxl import Workbook
-#wb = Workbook()
-#ws = wb.active
-#
-#GI=[]
-#for file in os.listdir("../res/blast_with_note/match/allhits/funcao_all_hits/"):
-#    if file.endswith(".txt"):
-#        limpo = file.replace(".txt", "")
-#        GI.append(limpo)
-#
-#index = 1             
-#for name in range(len(GI)):
-#    fic=open("../res/blast_with_note/match/allhits/funcao_all_hits/"+GI[name]+".txt").readlines()
-#    for i in range(len(fic)) :
-#        x=fic[i]
-#        ws['A' + str(index)] = GI[name]
-#        ws['B' + str(index)] = x[3:10]
-#        ws['C' + str(index)] = x[29:]
-#        index+=1
-#  
-#wb.save("withnote.xlsx")
-
+# Code to export results from blast to excel table
 import os.path
 from openpyxl import Workbook
 wb = Workbook()
 ws = wb.active
-
+#read all txt files from a foulder and return GI protein to a list
 GI=[]
 for file in os.listdir("../res/blast_without_note/match/allhits/funcao_all_hits/"):
     if file.endswith(".txt"):
         limpo = file.replace(".txt", "")
         GI.append(limpo)
 
+#open file with results of blast and export to excel table
 index = 1             
 for name in range(len(GI)):
     fic=open("../res/blast_without_note/match/allhits/funcao_all_hits/"+GI[name]+".txt").readlines()
