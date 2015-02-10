ab62b47939eb2ef253e4db8ac58c8b891acd5267
Danielbrag <danielbraga@live.com.pt>
2015-02-10 01:15:00 +0000

    Table with all functions for all hits
    order by our protein

--- src/get_protein_function.py
+++ src/get_protein_function.py
@@ -1,77 +1,47 @@
 # -*- coding: utf-8 -*-
 
-import os.path
-import pandas #creating data frame
-import numpy as np #creating array for data frame
-
-
-
+#import os.path
+#from openpyxl import Workbook
+#wb = Workbook()
+#ws = wb.active
 #
+#GI=[]
+#for file in os.listdir("../res/blast_with_note/match/allhits/funcao_all_hits/"):
+#    if file.endswith(".txt"):
+#        limpo = file.replace(".txt", "")
+#        GI.append(limpo)
 #
-#lista=[]        
+#index = 1             
 #for name in range(len(GI)):
-#    lista.append([])
-#    lista[name].append(GI[name])
 #    fic=open("../res/blast_with_note/match/allhits/funcao_all_hits/"+GI[name]+".txt").readlines()
-#    #data = fic.replace("\n", " ")
-#    for i in range(len(fic)):
-#        data = fic[i].replace("\n", " ")
-#        first = 'Gi: '
-#        last = '  Possivel'
-#        try:
-#            start = data.rindex( first ) + len( first )
-#            end = data.rindex(last, start)
-#            novo= data[start:end]
-#            lista[name].append(novo)
-#        except:
-#            pass       
-#
-#        first2 = 'função:   '
-#        last2 = '{ECO:'
-#        try:
-#            start2 = data.rindex( first2 ) + len( first2 )
-#            end2 = data.rindex(last2, start2)
-#            novo2= data[start2:end2]
-#            lista[name].append(novo2)
-#        except:
-#            pass       
+#    for i in range(len(fic)) :
+#        x=fic[i]
+#        ws['A' + str(index)] = GI[name]
+#        ws['B' + str(index)] = x[3:10]
+#        ws['C' + str(index)] = x[29:]
+#        index+=1
+#  
+#wb.save("withnote.xlsx")
 
+import os.path
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
 
-#
-#def tabela(l):
-#    nome=[]
-#    #dado=[]
-#    for i in range(len(lista)):
-#        #dado.append(lista[i][0])       
-#        nome.append(lista[i])
-#    data=np.array(nome)
-#    df=pandas.DataFrame(data)
-#    df.to_csv("../res/excel/funcao2", sep=',')
-#
-#print(tabela(lista))
-def func():
-    GI=[]
-    for file in os.listdir("../res/blast_with_note/match/allhits/funcao_all_hits/"):
-        if file.endswith(".txt"):
-            limpo = file.replace(".txt", "")
-            GI.append(limpo)
-            
-    lista=[]        
-    for name in range(len(GI)):
-        lista.append([])
-        lista[name].append(GI[name])
-        fic=open("../res/blast_with_note/match/allhits/funcao_all_hits/"+GI[name]+".txt").read()
-        lista[name].append(fic)
-    return lista
-def tabela():
-    lista=func()
-    nome=[]
-    #dado=[]
-    for i in range(len(lista)):
-        #dado.append(lista[i][0])       
-        nome.append(lista[i])
-    data=np.array(nome)
-    df=pandas.DataFrame(data)
-    df.to_csv("../res/excel/funcao2", sep=',')
+GI=[]
+for file in os.listdir("../res/blast_without_note/match/allhits/funcao_all_hits/"):
+    if file.endswith(".txt"):
+        limpo = file.replace(".txt", "")
+        GI.append(limpo)
 
-tabela()
+index = 1             
+for name in range(len(GI)):
+    fic=open("../res/blast_without_note/match/allhits/funcao_all_hits/"+GI[name]+".txt").readlines()
+    for i in range(len(fic)) :
+        x=fic[i]
+        ws['A' + str(index)] = GI[name]
+        ws['B' + str(index)] = x[3:10]
+        ws['C' + str(index)] = x[29:]
+        index+=1
+  
+wb.save("withoutnote.xlsx")
