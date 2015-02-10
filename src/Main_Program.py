55b128dedd1d7a8526c2223f1fd59f679200a42b
raqsilva <raqsilva8@hotmail.com>
2015-02-10 16:28:58 +0000

    updating Main program

--- src/Main_Program.py
+++ src/Main_Program.py
@@ -13,10 +13,9 @@ import numpy as np #creating array for data frame
 
 #[0:246000] my zone
 
+
 #fetching genome zone from Neisseria gonorrhoeae FA 1090 chromosome ncbi
 #VERSION: NC_002946.2   GI:59800473
-
-
 def get_genome_zone(start,stop,filename):
     Entrez.email = "pg27668@alunos.uminho.pt"
     handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", seq_start=start, seq_stop=stop, id="59800473")
@@ -28,7 +27,7 @@ def get_genome_zone(start,stop,filename):
     path=os.getcwd()
     src = path+"/"+filename #source folder
     dst = "../res"#destination folder
-    shutil.move(src, dst)
+    shutil.move(src, dst)#moving file to another directory
     record = SeqIO.read("../res/"+filename, "genbank") 
     return record
 
@@ -36,8 +35,8 @@ def get_genome_zone(start,stop,filename):
 #Get all the info from one gene
 def info(record,locus):
     lista=[]
-    for i in range(len(locus)):
-        lista.append([])
+    for i in range(len(locus)):#for all genes get the information from ncbi and create matrix
+        lista.append([])#each gene has a list of information
         lista[i].append(locus[i])
         lista[i].append(genes_names(record,locus[i]))
         lista[i].append(location(record,locus[i]))
@@ -49,11 +48,13 @@ def info(record,locus):
         lista[i].append(note(record,locus[i]))
     return lista
     
-    
+ 
+#accessing each list of gene info from matrix
 def aceder(lista,nr):
     return lista[nr-1]
 
 
+#creating data frame for posterior csv file
 def tabela(lista,locus):
     headers=['locus','gene name','location','GI GeneID','EC','proteinID','Uniprot_ID','product','note']
     dado=[]    
@@ -69,14 +70,14 @@ def protein_ID(record,locus_tag):
     for i in range(len(record.features)):
         my_cds = record.features[i]
         if my_cds.type == "CDS":
-            if my_cds.qualifiers["locus_tag"][0]==str(locus_tag):
+            if my_cds.qualifiers["locus_tag"][0]==str(locus_tag):#only accessing info from our genes
                 if "protein_id" in my_cds.qualifiers:
                     return my_cds.qualifiers["protein_id"][0]
                 else:
                     return "Nao contem protein_id"
 
 
-#Gives the sequence location in genome
+#Gives the sequence location and strand in genome
 def location(record,locus_tag):
     featcds = [ ]
     for i in range(len(record.features)):
@@ -87,7 +88,7 @@ def location(record,locus_tag):
         return record.features[k].location
                 
         
-#Notes from the sequence
+#Notes from the gene
 def note(record,locus_tag):
     for i in range(len(record.features)):
         my_cds = record.features[i]
@@ -143,7 +144,6 @@ def hypoth_proteins(record):
     return hypo
     
        
-#YP_009115478.1  
 #Gene ID and GI number
 def gene_ID_GI(record,locus_tag):
     for i in range(len(record.features)):
@@ -153,9 +153,10 @@ def gene_ID_GI(record,locus_tag):
             if my_gene.qualifiers["locus_tag"][0]==str(locus_tag):    
                 if "db_xref" in my_gene.qualifiers:
                     x=my_gene.qualifiers["db_xref"]
-                    return x[0],x[1]
+                    return x[0],x[1] #GI and GeneID
 
 
+#getting only GI number
 def GI_number(record,locus_tag):
     GI=[]
     for i in range(len(record.features)):
@@ -169,7 +170,7 @@ def GI_number(record,locus_tag):
     return GI
  
 
-#fetching CDD
+#fetching CDD for each gene from ncbi gb file
 def CDD(GI):
     CDD=[]
     for i in range(len(GI)):
@@ -180,7 +181,7 @@ def CDD(GI):
         handle.close()
         for j in range(len(record.features)):
             my_cds = record.features[j]
-            if my_cds.type == "Region" or my_cds.type == "Site":
+            if my_cds.type == "Region" or my_cds.type == "Site":#only feature type Region and Site has CDD
                 if "db_xref" in my_cds.qualifiers:
                     CDD[i].append(my_cds.qualifiers["db_xref"][0])
     return CDD    
@@ -231,6 +232,7 @@ def pseudogeneID(record,locus_tag):
                     return my_gene.qualifiers["db_xref"][0]
 
 
+#create matrix for all pseudogene info from ncbi gb file
 def info_pseudogenes(record,locus):
     lista=[]
     for i in range(len(locus)):
@@ -239,8 +241,9 @@ def info_pseudogenes(record,locus):
         lista[i].append(location_pseudo(record,locus[i]))
         lista[i].append(pseudogeneID(record,locus[i]))
     return lista   
-    
-
+  
+  
+#csv file
 def tabela_pseudogenes(lista,locus):
     headers=['locus','location','gene_ID']
     dado=[]    
@@ -350,7 +353,7 @@ def info_tRNA(record,locus):
     return lista   
 
 
-
+#csv file
 def tabela_tRNA(lista,locus):
     headers=['locus','gene_ID','product','location']
     dado=[]    
@@ -361,20 +364,21 @@ def tabela_tRNA(lista,locus):
     df.to_csv("../res/excel/teste_tRNA", sep='\t')
     
               
-
+#gets uniprot AC from uniprot using proteinID
 def uniprot_ID(record):
     locus=locus_tag(record)
     proteins=[]
     IDs=[]
     for i in range(len(locus)):
         proteins.append(protein_ID(record,locus[i]))
-    for j in range(len(proteins)):
+    for j in range(len(proteins)):#get uniprotAC for each proteinID from url
+        #the html page has in a consistent position the uniprotAC
         data = urllib.request.urlopen("http://www.uniprot.org/uniprot/?query="+proteins[j]+"&sort=score").read()
-        if len(data.split())>=3298:
+        if len(data.split())>=3298:#in our case this position consists to our uniprotAC
             x=str(data.split()[3298])
-            IDs.append(x[6:12])
+            IDs.append(x[6:12])#only append uniprotAC and not the other characters
         else:
-            IDs.append(proteins[j])
+            IDs.append(proteins[j])#if doesnt have UniprotAC append proteinID
     return IDs
 
 
@@ -383,23 +387,25 @@ def uniprot_ID(record):
 ID=['Q5FAJ2', 'Q5FAJ1', 'Q5FAJ0', 'Q5FAI9', 'Q5FAJ3', 'YP_009115477.1', 'Q5FAK0', 'Q5FAJ8', 'Q5FAJ7', 'Q5FAJ6', 'Q5FAJ5', 'Q5FAJ4', 'Q5FAL3', 'Q5FAL2', 'Q5FAL1', 'Q5FAK9', 'Q5FAK8', 'Q5FAK7', 'Q5FAK6', 'Q5FAK5', 'Q5FAI8', 'Q5FAI7', 'Q5FAI6', 'Q5FAI5', 'Q5FAI4', 'Q5FAI3', 'Q5FAI2', 'Q5FAI1', 'Q5FAH9', 'Q5FAH8', 'Q5FAH7', 'Q5FAH6', 'Q5FAH5', 'Q5FAH4', 'Q5FAH3', 'Q5FAH2', 'Q5FAH1', 'Q5FAH0', 'Q5FAG9', 'Q5FAG7', 'Q5FAG6', 'Q5FAG5', 'Q5FAG4', 'Q5FAG3', 'Q5FAG2', 'Q5FAG1', 'Q5FAG0', 'Q5FAF9', 'Q5FAF8', 'Q5FAF7', 'YP_008914846.1', 'Q5FAF6', 'Q5FAF5', 'Q5FAF4', 'Q5FAF3', 'Q5FAF2', 'Q5FAF1', 'Q5FAE8', 'Q5FAE7', 'Q5FAE6', 'Q5FAE5', 'Q5FAE4', 'Q5FAE3', 'Q5FAE2', 'Q5FAE1', 'Q5FAE0', 'Q5FAD9', 'Q5FAD8', 'Q5FAD7', 'Q5FAD6', 'Q5FAD5', 'Q5FAD4', 'Q5FAD3', 'Q5FAD2', 'Q5FAD1', 'Q5FAD0', 'Q5FAC9', 'Q5FAC8', 'Q5FAC7', 'Q5FAC6', 'Q5FAC5', 'Q5FAC4', 'Q5FAC3', 'Q5FAC2', 'Q5FAC1', 'Q5FAC0', 'Q5FAB9', 'Q5FAB7', 'Q5FAB6', 'Q5FAB5', 'Q5FAB4', 'Q5FAB3', 'Q5FAB1', 'Q5FAB0', 'Q5FAA9', 'Q5FAA8', 'Q5FAA7', 'Q5FAA6', 'Q5FAA5', 'Q5FAA4', 'Q5FAA3', 'Q5FAA2', 'YP_009115478.1', 'Q5FAA1', 'Q5FA99', 'Q5FA98', 'Q5FA97', 'Q5FA96', 'Q5FA94', 'Q5FA93', 'Q5FA92', 'Q5FA91', 'Q5FA90', 'Q5FA89', 'Q5FA88', 'Q5FA87', 'Q5FA86', 'Q5FA85', 'Q5FA84', 'Q5FA83', 'YP_008914847.1', 'Q5FA80', 'Q5FA78', 'YP_207322.2', 'Q5FA76', 'Q5FA75', 'Q5FA73', 'Q5FA72', 'Q5FA71', 'Q5FA70', 'Q5FA68', 'Q5FA67', 'Q5FA66', 'Q5FA65', 'Q5FA63', 'Q5FA62', 'Q5FA61', 'Q5FA60', 'Q5FA59', 'Q5FA58', 'Q5FA57', 'Q5FA56', 'Q5FA55', 'Q5FA54', 'Q5FA53', 'Q5FA52', 'Q5FA51', 'Q5FA50', 'Q5FA49', 'Q5FA48', 'Q5FA47', 'Q5FA46', 'Q5FA45', 'Q5FA44', 'Q5FA43', 'Q5FA42', 'Q5FA41', 'Q5FA40', 'Q5FA39', 'Q5FA38', 'Q5FA37', 'Q5FA36', 'Q5FA35', 'Q5FA34', 'Q5FA33', 'Q5FA32', 'Q5FA31', 'Q5FA30', 'Q5FA29', 'Q5FA28', 'Q5FA27', 'Q5FA26', 'Q5FA25', 'Q5FA24', 'Q5FA23', 'Q5FA22', 'Q5FA21', 'Q5FA20', 'Q5FA19', 'Q5FA18', 'Q5FA17', 'Q5FA15', 'Q5FA14', 'Q5FA13', 'Q5FA12', 'Q5FA11', 'Q5FA10', 'Q5FA09', 'Q5FA08', 'Q5FA07', 'Q5FA06', 'Q5FA05', 'Q5FA04', 'Q5FA03', 'Q5FA02', 'Q5FA01', 'Q5FA00', 'Q5F9Z9', 'Q5F9Z8', 'Q5F9Z7', 'Q5F9Z5', 'Q5F9Z4', 'Q5F9Z2']
 
 
+#this function uses other functions from Uniprot_Parser file
 def info_uniprot():
     identifier=[]
-    handle = open("../res/"+"uniprot.txt")
+    handle = open("../res/"+"uniprot.txt")#opens text file with all uniprot info from all proteins
     records = parse(handle) # Uses the function 'parse' from the module. 
     for record in records:
-        for i in range(len(ID)):
+        for i in range(len(ID)):#only gets info for our list of proteins
             identifier.append([])
             if record["AC"]==ID[i]+";":
                 #identifier.append([])
                 identifier[i].append(ID[i])
-                identifier[i].append(record["ID"])
-                identifier[i].append(record["DE"])
-                identifier[i].append(record["CC"])
+                identifier[i].append(record["ID"])#gives uniprotID, revision and length
+                identifier[i].append(record["DE"])#gives protein name
+                identifier[i].append(record["CC"])#gives comments on uniprot
     handle.close()
     return identifier
 
 
+#gets the other info from uniprot in the xml file
 def more_info_uniprot():
     handle = open("../res/"+"uniprot_XML.xml")
     records=UniprotIO.UniprotIterator(handle,return_raw_comments=True)
@@ -407,7 +413,7 @@ def more_info_uniprot():
     for record in records:
         for i in range(len(ID)):
             if record.id==ID[i]:
-                refs.append([ID[i]]+record.dbxrefs)#GO´s
+                refs.append([ID[i]]+record.dbxrefs)#GO´s, BioCyc, KO, ngo and others
     handle.close()
     return refs
     
@@ -470,6 +476,7 @@ def sorting(lista):
     return mat
 
 
+#get GO numbers from more_info_unitprot sorted matrix 
 def GO():
     l=[]
     lista=sorting(tab())
@@ -496,7 +503,6 @@ def DB_pubmed(gene):
     return idlist
 
 
-#745998704  
 #Running Blast and saving info into a file
 #GI_number - gene identification number
 def blast(GI_numb,filename):
@@ -536,7 +542,7 @@ def giwithout_note(record):
             if "note" not in my_cds.qualifiers:
                 x=my_cds.qualifiers["db_xref"]
                 gi=x[0]
-                ID.append(gi[3:])
+                ID.append(gi[3:])#only appends number and not the "GI:"
     return ID
 
 #blast gi without note    
@@ -762,6 +768,7 @@ def GInumbers(record,locus_tag):
                             GI.append(x[0])
     return GI
 
+
 #blast gi with note    
 def blastwithnote():
     locus=locus_tag(record)
@@ -779,7 +786,8 @@ def blastwithnote():
         src = path+"/"+GI_numb+'.xml' #source folder
         dst = "../res/blast_with_note/"#destination folder
         shutil.move(src, dst)
-                              
+            
+                  
 #return all hits from blast
 def allhitswithnote():
     lista=[]
@@ -821,11 +829,13 @@ lista_GO=[['Q5FAJ2', 'GO:GO:0003688', 'GO:GO:0005524', 'GO:GO:0005737',  'GO:GO:
 #get GO terms from URL
 def get_GO_terms(lista):
     nome=[]
-    for i in range(len(lista)):
+    for i in range(len(lista)):#for each uniprotAC create a list to append GO terms and numbers
         nome.append([])
-        for j in range(1,len(lista[i])):
-            GO=str(lista[i][j])
+        for j in range(1,len(lista[i])):#for each list append all info
+            GO=str(lista[i][j])#append GO number
+            #data gets the terms for that GO number
             data = urllib.request.urlopen("http://www.ebi.ac.uk/QuickGO/GTerm?id="+GO[3:13]).read()
+            #only appends the term from the html
             nome[i].append(data.split()[95])
             nome[i].append(data.split()[96])
             nome[i].append(data.split()[97])
@@ -840,7 +850,8 @@ G=[[b'<title>GO:0003688', b'DNA', b'replication', b'origin', b'binding</title>',
 #viewing GO and genes
 def which_GO(lista,G,pos):
    return lista[pos][0],G[pos]
-                  
+    
+              
 def blastanaliserwithnote():
     blast=[]    
     for file in os.listdir("../res/blast_with_note"):
